f5bfdc086caddec58292b6c0cc679211b8a668f0
paulnawat <scc385@gmail.com>
2018-05-22 23:48:01 -0700

    feat: enable lazy load for vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,9 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
 // views
-import Home from '@/views/home';
-import FlexBox from '@/views/flex-box';
-import Grid from '@/views/grid';
+const Home = () => import('@/views/home');
+const FlexBox = () => import('@/views/flex-box');
+const Grid = () => import('@/views/grid');
 
 Vue.use(Router);
 
