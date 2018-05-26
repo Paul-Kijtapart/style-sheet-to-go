a9a6678e07d5bec7932cfad80a99da9990ed115d
paulnawat <scc385@gmail.com>
2018-05-25 23:24:54 -0700

    feat: add grid layout with named column/rows

--- src/views/grid/grid.vue
+++ src/views/grid/grid.vue
@@ -27,9 +27,17 @@
         <footer class="grid-child grid-child--footer"> Footer</footer>
       </div>
 
-      <!-- Display Two -->
-      <div class="grid-parent--2">
-        <header class="grid-child grid-child--header"> Header</header>
+      <!-- Display Responsive 0 -->
+      <div class="grid-parent grid-parent--responsive-0">
+        <div v-for="item in items"
+             :key="item.id"
+             :class="['grid-child', 'grid-child--'+item.id]">
+          {{ item.label }}
+        </div>
+      </div>
+
+      <!-- Display 2 -->
+      <div class="grid-parent grid-parent--2">
         <aside class="grid-child grid-child--aside-1"> Aside 1</aside>
         <aside class="grid-child grid-child--aside-2">Aside 2</aside>
         <section class="grid-child grid-child--section"> Section</section>
@@ -93,6 +101,8 @@
   // wrapping around show case
   .grid-showcases {
 
+    padding: 10px;
+    background: black;
     display: grid;
 
     // Note: grid-gap is short-hand for Row and Column gap
@@ -109,6 +119,11 @@
 
   .grid-parent {
     overflow: hidden;
+    background: white;
+
+    // grid
+    display: grid;
+    grid-gap: 10px;
 
     //size
     height: 500px;
@@ -140,9 +155,6 @@
 
   // grid-parent-0
   .grid-parent--0 {
-
-    display: grid;
-    grid-gap: 10px;
     grid-template-columns: 100px 200px auto auto; // 4 columns
 
     .grid-child--a {
@@ -175,9 +187,7 @@
   // grid-parent 1
   .grid-parent--1 {
     // default layout to mobile screen
-    display: grid;
     grid-template-areas: "header" "section" "aside-1" "aside-2" "footer";
-    grid-gap: 10px;
 
     @media (min-width: 700px) {
       grid-template-areas: "header header header" "aside-1 section aside-2" "footer footer footer";
@@ -206,9 +216,81 @@
 
   }
 
-  // grid-parent 2
+  // grid-parent responsive 0
+  .grid-parent--responsive-0 {
+
+    display: grid;
+
+    // fr is like flex-grow
+    //(determine how available spaces get distributed among children)
+    grid-template-columns: 1fr 4fr 1fr;
+
+    .grid-child--a {
+      grid-row: 1/2;
+      grid-column: 1/-1;
+    }
+
+    .grid-child--b {
+      grid-row: 2/3;
+    }
+
+    .grid-child--c {
+      grid-row: 2/3;
+      grid-column: 2/3;
+    }
+
+    .grid-child--d {
+      grid-row: 2/3;
+      grid-column: 3/4;
+    }
+
+    .grid-child--e {
+      grid-row: 3/4;
+      grid-column: 1/3;
+    }
+
+    .grid-child--f {
+      grid-row: 3/4;
+      grid-column: 3/4;
+    }
+  }
+
+  // grid-parent responsive 1
   .grid-parent--2 {
 
+    // 3 named-columns (Best practice)
+    // IMPORTANT: it's possible to assign multiple names for each column [ name1, name2 ] 1fr (size of col)
+    grid-template-columns: [left-sidebar-start] 1fr [left-sidebar-end main-content-start] 4fr [main-content-end right-sidebar-start] 1fr [right-sidebar-end]; // this corrsponds to the GridLine
+
+    // 2 named-row (Best practice)
+    grid-template-rows: [body-start] 9fr [footer-start] 1fr;
+
+    .grid-child--aside-1 {
+      grid-row: body-start / span 1;
+      grid-column: left-sidebar-start / span 1;
+    }
+
+    .grid-child--aside-2 {
+      grid-row: body-start / span 1;
+      grid-column: right-sidebar-start / -1; // again - 1 = go to the end
+    }
+
+    .grid-child--section {
+      grid-row: body-start / span 1;
+
+      // We could
+      // grid-column: main-content-start / span 1;
+      grid-column: main-content-start / main-content-end; // VERBOSE!
+    }
+
+    .grid-child--footer {
+
+      // We could
+      // grid-column: left-sidebar-start / span 3; // span need to be followed # of columns this will take
+
+      // but this is better
+      grid-column: left-sidebar-start / right-sidebar-end;
+    }
   }
 
 </style>
